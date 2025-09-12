0a117df92603682e34c32a2038b881e085af5a1b
Eric Castro <castroheric@gmail.com>
2025-09-12 14:14:55 -0400

    fixu

--- resources/views/usuario/list.blade.php
+++ resources/views/usuario/list.blade.php
@@ -129,6 +129,7 @@
 @endsection
 
 @push('scripts')
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <script>
     document.addEventListener('DOMContentLoaded', function() {
         const permissionsModal = document.getElementById('permissionsModal');
