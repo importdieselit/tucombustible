2d8296e0fb3e0fb12b80c4d29eff7da9a6ab3f73
Eric Castro <castroheric@gmail.com>
2025-09-15 11:22:56 -0400

    newlog

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -37,4 +37,19 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+
+      /**
+     * Get the login username to be used by the controller.
+     *
+     * @return string
+     */
+    public function username()
+    {
+        // Se valida si el valor de entrada es un correo electrónico
+        $login = request()->input('email');
+        $field = filter_var($login, FILTER_VALIDATE_EMAIL) ? 'email' : 'name';
+        request()->merge([$field => $login]);
+        
+        return $field;
+    }
 }
