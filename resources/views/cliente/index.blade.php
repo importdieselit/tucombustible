af084ddef54bded72861dee561b7b8a7b7f77925
Eric Castro <castroheric@gmail.com>
2025-09-17 14:19:50 -0400

    suscursalDash

--- resources/views/cliente/index.blade.php
+++ resources/views/cliente/index.blade.php
@@ -416,6 +416,7 @@
     <script src="https://code.highcharts.com/highcharts.js"></script>
     <script src="https://code.highcharts.com/modules/exporting.js"></script>
     <script src="https://code.highcharts.com/modules/export-data.js"></script>
+    <script src="https://code.highcharts.com/modules/accessibility.js"></script>
     <script>
         document.addEventListener('DOMContentLoaded', function () {
             // Datos simulados pasados desde PHP
