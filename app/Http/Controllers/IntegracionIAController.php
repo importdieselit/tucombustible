459a4400243d8be870b7e2e0d1bdcc1dba4cba2c
Eric Castro <castroheric@gmail.com>
2025-10-03 14:46:36 -0400

    IAintegrate

--- app/Http/Controllers/IntegracionIAController.php
+++ app/Http/Controllers/IntegracionIAController.php
@@ -72,7 +72,7 @@ class IntegracionIAController extends Controller
 
      protected function identificarClientePorTelefono(Request $request)
     {
-        return response()->json(['success' => false, 'response' => 'Función en desarrollo.'], 501);
+        return response()->json(['success' => false, 'response' => json_decode($request)],200);
         // Botpress debe extraer el número y enviarlo en el payload
         $telefono = $request->input('telefono');
         
