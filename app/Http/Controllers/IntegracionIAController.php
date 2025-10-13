3d3bc8f07fc4bd6dc6acdd2508b395b0ef454e9c
Eric Castro <castroheric@gmail.com>
2025-10-13 12:31:20 -0400

    iaintg

--- app/Http/Controllers/IntegracionIAController.php
+++ app/Http/Controllers/IntegracionIAController.php
@@ -27,7 +27,7 @@ class IntegracionIAController extends Controller
         $userId = $request->input('userId'); 
         $telefono = $request->input('telefono'); 
 
-        \Log::info('📩 Llamada recibida desde Botpress:', $request->all());
+        Log::info('📩 Llamada recibida desde Botpress:', $request->all());
 
         Log::info("Webhook recibido. Acción: {$action}, telefono:{$telefono}. Usuario: {$userId}");
 
