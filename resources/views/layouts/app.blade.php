a41a85066fba8527be9cf4b3c361f77cb8c2ba15
Eric Castro <castroheric@gmail.com>
2025-10-21 11:22:45 -0400

    search

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -49,7 +49,8 @@
     <script src="https://code.jquery.com/jquery-3.7.0.min.js" xintegrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" xintegrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf198Ytg5eI4Nkz5q+0Ukn" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/js/all.min.js" xintegrity="sha512-rpLll8u6Jj6XvYhJ2kZlD+0ZlH2d9e6o4j5Yp7v0s3Ue5z7N46u5v6Z5q+0Ukn" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-        
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" xintegrity="sha384-I7E8VVD/ismYTF4y589a1H3eO/bpcGkYIId1D9g7aFwWp8zU2E/2lV8A4M4Z/B" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" xintegrity="sha384-0m1rA4j8C5bT2XqP2r2k8sH2wJ5xK0xL9Q9z9n0B4E8G5J/B7V7P" crossorigin="anonymous"></script>
     
     <script src="{{ mix('js/app.js') }}"></script>
     <script src="{{ asset('js/alerts.js') }}"></script>
