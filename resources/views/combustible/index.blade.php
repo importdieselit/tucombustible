9a346d09632cabbb81a38169aa81093b56c328d4
Eric Castro <castroheric@gmail.com>
2025-10-21 12:36:47 -0400

    fixaobs

--- resources/views/combustible/index.blade.php
+++ resources/views/combustible/index.blade.php
@@ -489,7 +489,7 @@
                                 <p class="text-muted m-0"><small>Nivel: {{ $percentage }}%</small></p>
                                 <p class="text-black m-0 "><small>{{ $deposito->nivel_actual_litros }}/{{ $deposito->capacidad_litros }} Litros</small>  
                                     <i class="rounded fa fa-pencil ajustar-btn" style="cursor: pointer" data-id="{{$deposito->id}}" onclick="openAjusteModal({{$deposito->id}})"></i>
-                                    <a href="{{ route('depositos.aforo.show', ['deposito' => $deposito->id]) }}">
+                                    <a href="{{ route('depositos.aforo.show', ['deposito' => $deposito->id]) }}" target="_blank">
                                         <i class="rounded fa fa-table"></i>
                                     </a>
                                 </p>    
