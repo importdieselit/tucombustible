337eb52cc390ede1b147761e35b3577b6ce7dfef
Eric Castro <castroheric@gmail.com>
2025-10-23 10:02:00 -0400

    calendat

--- resources/views/viajes/calendario.blade.php
+++ resources/views/viajes/calendario.blade.php
@@ -59,6 +59,10 @@
     <script src="https://cdn.jsdelivr.net/npm/html2canvas@1.4.1/dist/html2canvas.min.js"></script> 
     <script>
         @once('fullcalendar')
+            const printableArea = document.querySelector('.printableArea');
+            const statusMessage = document.getElementById('statusMessage');
+            const outputContainer = document.getElementById('outputContainer');
+            
         document.addEventListener('DOMContentLoaded', function() {
 
              async function CopyToClipboard() {
@@ -357,6 +361,7 @@
         console.error('Error al inicializar el calendario o parsear JSON:', e);
     }
 });
+@endonce
     </script>
 @endpush
 
