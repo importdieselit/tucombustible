5ee4ead37eeaacdc22546d5eb1d8f1f01f94d883
Eric Castro <castroheric@gmail.com>
2025-11-03 10:29:25 -0400

    moduloCompras

--- app/Models/Planta.php
+++ app/Models/Planta.php
@@ -4,8 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\TabuladorViatico;
+use App\Models\Proveedor;
 
 class Planta extends Model
 {
     use HasFactory;
+
+    protected $table = 'plantas';
+    protected $fillable = ['nombre', 'direccion', 'telefono', 'alias', 'proveedor', 'id_tabulador_viatico'];
+    
+
+    public function tabuladorViatico()
+    {
+        return $this->belongsTo(TabuladorViatico::class, 'id_tabulador_viatico', 'id');
+    }
+
+    public function proveedor()
+    {
+        return $this->belongsTo(Proveedor::class, 'proveedor', 'id');
+    }
+
 }
