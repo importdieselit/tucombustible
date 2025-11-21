d706ad58f7d9b59bde51d38e25d077ff7ece60a9
Eric Castro <castroheric@gmail.com>
2025-11-21 11:42:42 -0400

    incidencia

--- resources/views/orden/compras.blade.php
+++ resources/views/orden/compras.blade.php
@@ -58,7 +58,7 @@
                         if(!isset($orden)){
                             $orden=$po->orden()->first();
                         }
-                        dd($orden);
+                        //dd($orden);
                     @endphp
                     <tr class="clickable-row" data-orden="{{ $orden->id }}" data-id="{{ $po->id }}">
                         <td>{{ $orden->id }}</td>
