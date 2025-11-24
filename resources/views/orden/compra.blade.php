70968a85285d4ac633eb8418411533031f7a34ee
Eric Castro <castroheric@gmail.com>
2025-11-24 10:22:43 -0400

    sumoinsitros

--- resources/views/orden/compra.blade.php
+++ resources/views/orden/compra.blade.php
@@ -32,23 +32,25 @@
             Procesando...
         </div>
     <div id="reporte-area" class="card shadow-sm p-3 bg-white border border-primary printableArea" >
+        <div class="row">
+            <div class="col col-6 text-left">
+                <img src="{{ asset('img/logo1.png') }}" alt="logo empresa" style="width: 250px">
+            </div>
+            <div class="col col-6 text-right">
+                <p class="text-muted small mb-1">Fecha de Emisión: {{ now()->format('d/m/Y H:i') }}</p>
+            </div>
         
         <!-- Encabezado del Reporte Simplificado -->
-        <div class="text-center mb-3">
-            <h5 class="text-primary fw-bold mb-0">Requerimiento de Suministros</h5>
-            <p class="text-muted small mb-1">Fecha de Emisión: {{ now()->format('d/m/Y H:i') }}</p>
-            {{-- <div class="bg-light p-2 rounded d-inline-block border border-danger">
-                <span class="fw-light small me-2">Presupuesto Total Estimado:</span> 
-                <span class="text-danger fw-bold">$ {{ number_format($totalViaticosPresupuestados, 2) }}</span>
-            </div> --}}
+            <div class="col-12 text-center mb-3">
+                <h5 class="text-primary fw-bold mb-0">Requerimiento de Suministros</h5>
+            </div>
         </div>
-
         <!-- Tabla de Detalle Simplificada -->
         <div class="table-responsive" style="max-height: 500px; overflow-y: auto;">
             <table class="table table-sm" style="font-size: 0.75rem;">  
                 <thead class="bg-primary text-white">
                     <tr style="font-weight: 700">
-                        <th class="py-1" colspan="2"><img src="{{ asset('img/logo1.png') }}" alt="logo empresa" style="width: 250px"></th>
+                        <th class="py-1" colspan="2"></th>
                         <th class="py-1" width="30%"></th>
                     </tr>
                     <tr>
