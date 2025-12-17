33d224fc77188735d1ded54c3126dad95dc6a303
Eric Castro <castroheric@gmail.com>
2025-12-17 15:38:29 -0400

    boletas

--- resources/views/combustible/guia.blade.php
+++ resources/views/combustible/guia.blade.php
@@ -134,13 +134,13 @@
             </div>
             <div class="col-md-6">
                 <label>Cliente / RIF</label>
-                <input type="text" id="rif_input" class="form-control hybrid-autocomplete" 
+                <input type="text" id="cliente_rif_input" class="form-control hybrid-autocomplete" 
                     data-db-field="cliente_rif" data-live-id="#live-cliente-rif" 
                     value="{{ $viaje->despachos->first()->cliente->rif ?? '' }}" placeholder="Escriba para buscar o crear...">
             </div>
             <div class="col-md-6">
                 <label>Cliente / direccion</label>
-                <input type="text" id="direccion_input" class="form-control hybrid-autocomplete" 
+                <input type="text" id="cliente_direccion_input" class="form-control hybrid-autocomplete" 
                     data-db-field="cliente_direccion" data-live-id="#live-cliente-direccion" 
                     value="{{ $viaje->despachos->first()->cliente->direccion ?? '' }}" placeholder="Escriba para buscar o crear...">
             </div>
@@ -359,11 +359,11 @@
                 $(liveId).text(ui.item.value); 
                 if (dbField === 'cliente_nombre') {
                     $('#cliente_rif_input').val(ui.item.rif);
-                    $('#cliente_dir_input').val(ui.item.direccion);
+                    $('#cliente_direccion_input').val(ui.item.direccion);
                     
                     // Actualizar también la vista previa de esos campos
                     $('#live-cliente-rif').text(ui.item.rif);
-                    $('#live-cliente-dir').text(ui.item.direccion);
+                    $('#live-cliente-direccion').text(ui.item.direccion);
                     
                     // Guardar todo el bloque del cliente
                     saveToDatabase('cliente_full_update', {
