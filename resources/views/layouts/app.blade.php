97acdc829515d143da463195851ea9a3e9ff2d7e
Eric <castroheric@gmail.com>
2026-02-11 08:49:26 -0400

    addShowandEditUSers_addFavicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,7 @@
     <!-- Font Awesome CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" xintegrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2C22gB7Fz2i4M8c9tU8vQ+I6bLwK6z+a6D+Q==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
-    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     @stack('styles')
 </head>
 <body>
@@ -103,7 +103,7 @@
         <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" xintegrity="sha384-I7E8VVD/ismYTF4y589a1H3eO/bpcGkYIId1D9g7aFwWp8zU2E/2lV8A4M4Z/B" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" xintegrity="sha384-0m1rA4j8C5bT2XqP2r2k8sH2wJ5xK0xL9Q9z9n0B4E8G5J/B7V7P" crossorigin="anonymous"></script>
     
-    <script src="{{ mix('js/app.js') }}"></script>
+    <script src="{{ asset('js/app.js') }}"></script>
     <script src="{{ asset('js/alerts.js') }}" defer></script>
     <script src="{{ asset('js/jquery.PrintArea.js') }}"></script>
     
