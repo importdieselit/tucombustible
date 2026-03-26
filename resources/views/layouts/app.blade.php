6d1374242d05537e24a4f6b1ed4f074eceeb90e8
Eric <castroheric@gmail.com>
2026-03-26 09:08:40 -0400

    fixpwa

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="theme-color" content="#4e73df">
     <link rel="manifest" href="{{ asset('manifest.json') }}">
-    {{-- <link rel="apple-touch-icon" href="{{ asset('img/icon-192x192.png') }}"> --}}
+    <link rel="apple-touch-icon" href="{{ asset('img/icon-192x192.png') }}">
     <title>@yield('title', 'Dashboard - TuCombustible')</title>
 
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
