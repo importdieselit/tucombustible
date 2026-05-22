14a9049507f3eaf1befabfe75c5777d98cb71032
EricICH <castroheric@gmail.com>
2026-05-22 12:50:31 -0400

    fixmapstyle

--- resources/views/cliente/rastreo.blade.php
+++ resources/views/cliente/rastreo.blade.php
@@ -138,7 +138,7 @@
                     fullscreenControl: true,
                 }).setView([lat, lng], 14);
 
-                L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                 L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
                     maxZoom: 19,
                     attribution: '© OpenStreetMap'
                 }).addTo(map);
