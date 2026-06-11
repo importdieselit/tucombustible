cdbf6e950726c20d018b07743d16c8307dfd4b0c
EricICH <castroheric@gmail.com>
2026-06-11 16:37:40 -0400

    pagos

--- app/Console/Commands/WatchAdmonReports.php
+++ app/Console/Commands/WatchAdmonReports.php
@@ -166,9 +166,10 @@ class WatchAdmonReports extends Command
                 $numerosDestino = [
                     '584241666291', // Jefe
                     '584241177910', // Gerente
-                    '584242982588' // admin
-                   // '584143779488', //comercial
-                    //'584141780355'  // Leudo
+                    '584242982588', // admin
+                    '584242542791', // admin2
+                    '584143779488', //comercial
+                    '584141780355'  // Leudo
                 ];
 
                 foreach ($numerosDestino as $numero) {
