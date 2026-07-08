0e36000abccc7121c9a81ddc58326384b38b3e26
EricICH <castroheric@gmail.com>
2026-07-08 15:54:51 -0400

    fixReporte

--- resources/views/reports/admon.blade.php
+++ resources/views/reports/admon.blade.php
@@ -294,6 +294,7 @@
                                     <tr><th>Cliente / Cuenta</th><th class="text-end">Monto (USD)</th></tr>
                                 </thead>
                                 <tbody>
+                                    {{  dd($cxpDesglose) }}
                                     @foreach($cxcDesglose as $cxc)
                                         @if($cxc->monto != 0)
                                         <tr>
