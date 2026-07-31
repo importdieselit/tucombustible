d7557ad0895985d0bf7652c8f3fdbe944ec027a5
EricICH <castroheric@gmail.com>
2026-07-31 12:33:56 -0400

    fix

--- app/Console/Commands/VerificarChequeoHorario.php
+++ app/Console/Commands/VerificarChequeoHorario.php
@@ -54,7 +54,7 @@ class VerificarChequeoHorario extends Command
                 $mensaje .= "⚠️ Por favor, proceder con el varillaje y la carga de datos en el sistema a la brevedad.";
 
                 try {
-                    $this->whatsappService->enviarMensaje($idDestino, $mensaje);
+                    $this->whatsappService->enviarMensaje($mensaje, $idDestino);
                     $this->info("Alerta enviada para la sede: {$sede->nombre}");
                 } catch (Exception $e) {
                     logger()->error("Fallo al enviar recordatorio de horario a WhatsApp: " . $e->getMessage());
