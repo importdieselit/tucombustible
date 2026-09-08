73ace95ff53af99453de0b7c4480cf367ab57efa
EricICH <castroheric@gmail.com>
2026-09-08 14:52:42 -0400

    OutVehicule

--- routes/web.php
+++ routes/web.php
@@ -551,6 +551,11 @@ Route::middleware(['auth'])->group(function () {
             Route::get('/sobreconsumo', [LogisticaController::class, 'sobreconsumo'])->name('sobreconsumo');
             Route::get('/crear/{tipo?}', [LogisticaController::class, 'create'])->name('create');
             Route::post('/guardar', [LogisticaController::class, 'store'])->name('store');
+
+            // Búsqueda AJAX de coincidencia de nombres y creación rápida desde Planificación
+            Route::get('/buscar-similares', [LogisticaController::class, 'buscarClientesSimilares'])->name('clientes.similares');
+            Route::post('/clientes/store-rapido', [LogisticaController::class, 'storeClienteRapido'])->name('clientes.store_rapido');
+
             // Rutas para Edición
             Route::get('/{id}', [LogisticaController::class, 'show'])->name('show');
             Route::get('/{id}/editar', [LogisticaController::class, 'edit'])->name('edit');
